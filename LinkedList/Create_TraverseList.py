4d360e89e4e575f05e2fce65633f689c1053859b
RakibulIslamRakib <56424436+RakibulIslamRakib@users.noreply.github.com>
2021-08-20 21:53:06 -0700

    Add files via upload

---
+++ LinkedList/Create_TraverseList.py
@@ -0,0 +1,40 @@
+class Node():
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+    
+
+dataArray = list(map(int,input().split()))
+
+headNode=None;currentNode=None;
+for el in dataArray:
+    newNode = Node(el)
+    if headNode is None:
+        headNode = newNode;
+        currentNode = newNode;
+        continue
+    currentNode.next = newNode
+    currentNode = currentNode.next
+
+tr = headNode
+while True:
+    print('Node Data : ',tr.data)
+    print('Node Address : ',tr)
+    if tr.next is None:
+        break
+    tr=tr.next
+
+    '''
+    9 7 5 3 4
+    Node Data :  9
+    Node Address :  <__main__.Node object at 0x000002843DD56FD0>
+    Node Data :  7
+    Node Address :  <__main__.Node object at 0x000002843DD56F10>
+    Node Data :  5
+    Node Address :  <__main__.Node object at 0x000002843DD56E80>
+    Node Data :  3
+    Node Address :  <__main__.Node object at 0x000002843DD56E20>
+    Node Data :  4
+    Node Address :  <__main__.Node object at 0x000002843DD56DC0>
+    '''
+
